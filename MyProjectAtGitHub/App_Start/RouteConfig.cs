3e6d2111c4c3fa8c3d75723012c024891496c06a
maayan <maayany180@gmail.com>
2019-02-11 18:02:52 +0200

    one commit

--- MyProjectAtGitHub/App_Start/RouteConfig.cs
+++ MyProjectAtGitHub/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyProjectAtGitHub
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Start", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Search", id = UrlParameter.Optional }
             );
         }
     }
--- MyProjectAtGitHub/Controllers/HomeController.cs
+++ MyProjectAtGitHub/Controllers/HomeController.cs
@@ -8,17 +8,38 @@ namespace MyProjectAtGitHub.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public ActionResult Search()//search page
         {
             ViewBag.Title = "Home Page";
-
+            Session["BookMark"] = " ";
             return View();
         }
 
-        public ActionResult Start()
+        public ActionResult AddBookMark(string ID)//Add to bookmark
         {
-            ViewBag.Title = "Home Page";
+           
+           if(Session["BookMark"] == null  || Session["BookMark"].GetType() != typeof(Dictionary<string, string>))
+            {
+                Session["BookMark"] =new Dictionary<string, string>();
+            }
 
+            ((Dictionary<string, string>)Session["BookMark"]).Add(ID,"https://github.com/"+ID);
+
+
+            return Json(new object(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult action()//Viewing bookmarks
+        {
+            if (Session["BookMark"].ToString()==" ")
+            {
+                ViewBag.note = "Once you add repositories to your bookmark, you can view them using the links shown here";
+            }
+            else
+            {
+                ViewBag.note = "Links to selected repositories";
+            }
             return View();
         }
 
